c55c283ed1ee1563ccfa9c30a02b8dc1162a1b23
Junho <51792037+ojh050118@users.noreply.github.com>
2021-11-07 17:41:37 +0900

    Remove unnessary comments

--- Circle.Game/CircleGame.cs
+++ Circle.Game/CircleGame.cs
@@ -11,8 +11,6 @@ namespace Circle.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            // Add your top-level game components here.
-            // A screen stack and sample screen has been provided for convenience, but you can replace it if you don't want to use screens.
             Child = screenStack = new ScreenStack { RelativeSizeAxes = Axes.Both };
         }
 
