734988f90ebfd02a91e5b7eca5f1769a11c2081a
Junho <51792037+ojh050118@users.noreply.github.com>
2021-11-11 00:37:52 +0900

    Add `CircleSliderBar`, Change userinterface to be consistent, Remove unnessary members

---
+++ Circle.Game.Tests/Visual/UserInterface/TestSceneCircleSliderBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Circle.Game.Graphics.UserInterface;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+
+namespace Circle.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneCircleSliderBar : CircleTestScene
+    {
+        private BindableNumber<float> value;
+
+        public TestSceneCircleSliderBar()
+        {
+            value = new BindableNumber<float>()
+            {
+                MinValue = 0,
+                MaxValue = 100,
+            };
+
+            Add(new CircleSliderBar<float>()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.X,
+                KeyboardStep = 5,
+                Current = value
+            });
+        }
+    }
+}
---
+++ Circle.Game.Tests/Visual/UserInterface/TestSceneSettingsSlider.cs
@@ -0,0 +1,33 @@
+﻿using Circle.Game.Screens.Setting;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osuTK.Graphics;
+
+namespace Circle.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneSettingsSlider : CircleTestScene
+    {
+        private BindableNumber<float> value;
+
+        public TestSceneSettingsSlider()
+        {
+            value = new BindableNumber<float>()
+            {
+                MinValue = 0,
+                MaxValue = 100,
+            };
+
+            Add(new Box { RelativeSizeAxes = Axes.Both, Colour = Color4.DarkGray });
+            Add(new SettingsSlider<float>
+            {
+                LeftIcon = FontAwesome.Solid.VolumeDown,
+                RightIcon = FontAwesome.Solid.VolumeUp,
+                Text = "volume",
+                Current = value,
+                KeyboardStep = 5
+            });
+        }
+    }
+}
