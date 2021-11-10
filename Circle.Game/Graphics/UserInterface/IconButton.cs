c8b7de5b80d941655f205283dc28134d36a45d3e
Junho <51792037+ojh050118@users.noreply.github.com>
2021-11-11 00:36:51 +0900

    Add `CircleSliderBar`, Change userinterface to be consistent, Remove unnessary members

---
+++ Circle.Game/Graphics/UserInterface/IconButton.cs
@@ -0,0 +1,32 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osuTK;
+
+namespace Circle.Game.Graphics.UserInterface
+{
+    public class IconButton : CircleButton
+    {
+        public const float DEFAULT_BUTTON_SIZE = 30;
+
+        public IconUsage Icon
+        {
+            get => icon.Icon;
+            set => icon.Icon = value;
+        }
+
+        private readonly SpriteIcon icon;
+
+        public IconButton(bool useBackground = true)
+            : base(useBackground)
+        {
+            Size = new Vector2(DEFAULT_BUTTON_SIZE);
+            Content.Add(icon = new SpriteIcon
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Size = new Vector2(0.6f)
+            });
+        }
+    }
+}
