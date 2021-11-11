e888f2555e2cefe217b3a2f86ea6365353d8f04f
Junho <51792037+ojh050118@users.noreply.github.com>
2021-11-11 16:03:04 +0900

    Remove unnessary using

--- Circle.Game/Screens/Setting/Sections/AudioSection.cs
+++ Circle.Game/Screens/Setting/Sections/AudioSection.cs
@@ -1,10 +1,7 @@
-﻿using Circle.Game.Configuration;
-using Circle.Game.Graphics.UserInterface;
-using osu.Framework.Allocation;
+﻿using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Platform;
 
 namespace Circle.Game.Screens.Setting.Sections
 {
