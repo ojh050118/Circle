aa9a911343309a426aa3fc21074c334edee02df3
오준호 <ojh0050118@gmail.com>
2022-01-31 11:41:31 +0900

    Fix CI

--- Circle.Game/Rulesets/Objects/BasicTile.cs
+++ Circle.Game/Rulesets/Objects/BasicTile.cs
@@ -1,10 +1,8 @@
-﻿using System.Globalization;
-using osu.Framework.Extensions.Color4Extensions;
+﻿using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
 using osuTK.Graphics;
 
 namespace Circle.Game.Rulesets.Objects
