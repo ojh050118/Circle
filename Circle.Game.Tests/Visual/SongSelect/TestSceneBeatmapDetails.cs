55389e414281b3c17782ecfde998d4388ffbaa70
오준호 <ojh0050118@gmail.com>
2022-02-03 14:31:21 +0900

    Use `TextFlowContainer` instead of `SpriteText` for beatmap description

---
+++ Circle.Game.Tests/Visual/SongSelect/TestSceneBeatmapDetails.cs
@@ -0,0 +1,21 @@
+﻿using Circle.Game.Beatmap;
+using Circle.Game.Screens.Select;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+
+namespace Circle.Game.Tests.Visual.SongSelect
+{
+    public class TestSceneBeatmapDetails : CircleTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load(BeatmapStorage beatmaps)
+        {
+            BeatmapDetails details;
+
+            Add(details = new BeatmapDetails { Padding = new MarginPadding(10) });
+            AddLabel("Beatmaps");
+            foreach (var beatmap in beatmaps.GetBeatmaps())
+                AddStep($"Change to {beatmap.Settings.SongFileName}", () => details.ChangeBeatmap(beatmap));
+        }
+    }
+}
