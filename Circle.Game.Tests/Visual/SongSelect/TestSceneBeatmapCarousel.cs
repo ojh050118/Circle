13f7dd9a5b46ce4153b32ba930c381c477461359
오준호 <ojh0050118@gmail.com>
2022-02-20 18:25:39 +0900

    Add more test step

--- Circle.Game.Tests/Visual/SongSelect/TestSceneBeatmapCarousel.cs
+++ Circle.Game.Tests/Visual/SongSelect/TestSceneBeatmapCarousel.cs
@@ -2,20 +2,31 @@
 using osu.Framework.Graphics.Shapes;
 using osuTK.Graphics;
 using osu.Framework.Graphics;
+using osu.Framework.Allocation;
+using Circle.Game.Beatmap;
 
 namespace Circle.Game.Tests.Visual.SongSelect
 {
     public class TestSceneBeatmapCarousel : CircleTestScene
     {
-        public TestSceneBeatmapCarousel()
+        [BackgroundDependencyLoader]
+        private void load(BeatmapStorage beatmaps)
         {
+            BeatmapCarousel carousel;
+
             Add(new Box
             {
                 Colour = Color4.Black,
                 Alpha = 0.7f,
                 RelativeSizeAxes = Axes.Both,
             });
-            Add(new BeatmapCarousel());
+            Add(carousel = new BeatmapCarousel());
+            AddLabel("Select beatmap(vertical direction)");
+            AddRepeatStep("Select beatmap(down)", () => carousel.SelectBeatmap(VerticalDirection.Down), 5);
+            AddRepeatStep("Select beatmap(up)", () => carousel.SelectBeatmap(VerticalDirection.Up), 5);
+            AddLabel("Select beatmap(beatmap)");
+            foreach (var beatmap in beatmaps.GetBeatmaps())
+                AddStep($"Select beatmap({beatmap.Settings.Song})", () => carousel.SelectBeatmap(beatmap));
         }
     }
 }
