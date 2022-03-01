cefe8f8a6e490ff67dc0c1ad7aeca96955569725
Junho <51792037+ojh050118@users.noreply.github.com>
2022-03-01 13:22:45 +0900

    Remove unused field

--- Circle.Game.Tests/CircleTestBrowser.cs
+++ Circle.Game.Tests/CircleTestBrowser.cs
@@ -18,7 +18,6 @@ namespace Circle.Game.Tests
     public class CircleTestBrowser : CircleGameBase
     {
         private Background background;
-        private VolumeOverlay volume;
         private ImportOverlay import;
         private DialogOverlay dialog;
         private Toast toast;
