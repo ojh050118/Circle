ab8bd5629e06cc8d5b7bb895810fd5c6ab632704
오준호 <ojh0050118@gmail.com>
2022-03-26 00:23:33 +0900

    Fix CI

--- Circle.Game/Screens/Select/SongSelectScreen.cs
+++ Circle.Game/Screens/Select/SongSelectScreen.cs
@@ -60,7 +60,6 @@ namespace Circle.Game.Screens.Select
                         }
                     }
                 },
-                
             };
 
             if (beatmapManager.LoadedBeatmaps == null)
