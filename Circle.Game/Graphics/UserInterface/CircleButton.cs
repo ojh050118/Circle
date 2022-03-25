39e37820e0918451e42e1e0f469f0630bc0d345a
오준호 <ojh0050118@gmail.com>
2022-03-26 01:15:27 +0900

    Fix background visibility

--- Circle.Game/Graphics/UserInterface/CircleButton.cs
+++ Circle.Game/Graphics/UserInterface/CircleButton.cs
@@ -16,6 +16,8 @@ namespace Circle.Game.Graphics.UserInterface
         private Sample hoverSample;
         private Sample clickSample;
 
+        private bool useBackground;
+
         protected new Container Content;
 
         public new float CornerRadius
@@ -26,6 +28,7 @@ namespace Circle.Game.Graphics.UserInterface
 
         public CircleButton(bool useBackground = true)
         {
+            this.useBackground = useBackground;
             Child = Content = new Container
             {
                 Anchor = Anchor.Centre,
@@ -36,8 +39,7 @@ namespace Circle.Game.Graphics.UserInterface
                 Child = box = new Box
                 {
                     RelativeSizeAxes = Axes.Both,
-                    Colour = Color4.Black.Opacity(0.4f),
-                    Alpha = useBackground ? 1 : 0
+                    Colour = Color4.Black.Opacity(useBackground ? 0.4f : 0),
                 }
             };
         }
@@ -62,8 +64,7 @@ namespace Circle.Game.Graphics.UserInterface
         {
             base.OnHoverLost(e);
 
-            box.FadeColour(Color4.Black.Opacity(0.4f), 500, Easing.OutQuint);
-
+            box.FadeColour(Color4.Black.Opacity(useBackground ? 0.4f : 0), 500, Easing.OutQuint);
         }
 
         protected override bool OnMouseDown(MouseDownEvent e)
