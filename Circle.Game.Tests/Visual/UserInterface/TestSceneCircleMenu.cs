d3f1c6342ead0def7841c1f3e14b1aef40d9eb43
ojh050118 <ojh0050118@gmail.com>
2022-05-28 15:43:11 +0900

    Match `CircleMenuItem` to current design

---
+++ Circle.Game.Tests/Visual/UserInterface/TestSceneCircleMenu.cs
@@ -0,0 +1,25 @@
+﻿using Circle.Game.Graphics.UserInterface;
+using osu.Framework.Graphics;
+using osuTK;
+using osuTK.Graphics;
+
+namespace Circle.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneCircleMenu : CircleTestScene
+    {
+        public TestSceneCircleMenu()
+        {
+            Add(new CircleMenu(Direction.Vertical, true)
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Items = new[]
+                {
+                    new CircleMenuItem("standard", MenuItemType.Standard),
+                    new CircleMenuItem("highlighted", MenuItemType.Highlighted),
+                    new CircleMenuItem("destructive", MenuItemType.Destructive),
+                }
+            });
+        }
+    }
+}
